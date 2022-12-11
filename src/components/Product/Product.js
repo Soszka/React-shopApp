fb25215d5e00361911e0fd35cca82574e8b73be3
Bartlomiej Socha <ziomek2628@wp.pl>
2022-12-11 09:17:08 +0100

    Third stage of project finished

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -8,6 +8,10 @@ const Product = props => {
   const [currentColor, setCurrentColor] = useState(props.colors[0])
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name)
 
+  const prepaerColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
@@ -25,18 +29,13 @@ const Product = props => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map(size => <li onClick={() => {setCurrentSize(size.name)}} key={size.name}><button type="button" className={currentSize == size.name && styles.active}>{size.name}</button></li>)}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors.map(color => <li key={color} onClick={() => {setCurrentColor(color)}}><button type="button" className={clsx(prepaerColorClassName(color), color === currentColor && styles.active)}></button></li>)}
             </ul>
           </div>
           <Button className={styles.button}>

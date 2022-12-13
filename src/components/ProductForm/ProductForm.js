8b737cc0d983637fb296a251223a594b50365c48
Soszka <bsocha1998@gmail.com>
2022-12-13 17:21:22 +0100

    useMemo added

--- src/components/ProductForm/ProductForm.js
+++ src/components/ProductForm/ProductForm.js
@@ -7,7 +7,7 @@ import shortid from 'shortid';
 const ProductForm = props => {
     return (
         <form onSubmit={props.addToCart}>
-          <OptionSize key={shortid()} setCurrentSize={props.setCurrentSize} currentSize={props.CurrentSize} sizes={props.sizes}/>
+          <OptionSize key={shortid()} setCurrentSize={props.setCurrentSize} currentSize={props.currentSize} sizes={props.sizes}/>
           <OptionColor key={shortid()} setCurrentColor={props.setCurrentColor} currentColor={props.currentColor} colors={props.colors}/>
           <Button className={styles.button}><span className="fa fa-shopping-cart" /></Button>
         </form>
